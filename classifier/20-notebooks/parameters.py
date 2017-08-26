c4c557e912576139626fb5fb2d123f784b08e91f
heavy02011 <you@example.com>
2017-08-26 06:55:04 +0200

    docs: training notebook updated

---
+++ classifier/20-notebooks/parameters.py
@@ -0,0 +1,19 @@
+## dirs confs
+DATASET_FOLDER="./dataset"
+MODELS_CHECKPOINTS_DIR="./checkpoints"
+
+# training confs
+#BATCH_SIZE = 64
+#TRAINING_EPOCHS = 200 #max
+#TRAIN_IMAGES_PER_EPOCH = 16768
+#VALIDATE_IMAGES_PER_EPOCH = 1856
+IMAGE_WIDTH = 224
+IMAGE_HEIGHT = 224
+
+# hyperparameters
+batch_size = 64 #8 #64
+nb_epoch = 25 #50 #25 #3 #25
+nb_train_samples = 2000
+
+# save file into directory download
+save_img = False
